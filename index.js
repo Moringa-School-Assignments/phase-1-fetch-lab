05d63948399b9b2adde9933249f599d48ab862c2
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-08-29 17:51:08 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  let books = fetch("https://anapioficeandfire.com/api/books")
+  .then((res) => res.json())
+  .then(data => renderBooks(data));
+  return books
 }
 
 function renderBooks(books) {
